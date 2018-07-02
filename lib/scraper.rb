0eba52f934b7613cb8cbd72b1319073ed8ce3a65
Seth Wax <sethlwax@gmail.com>
2018-07-02 13:06:21 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,6 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-
     Array.new.tap do |students_array|
       doc.css(".student-card").each do |student|
         students_array << {
@@ -19,7 +18,6 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-
     Hash.new.tap do |student_hash|
       doc.css(".social-icon-container a").each do |container|
         case link = container.attribute("href").value
@@ -33,7 +31,6 @@ class Scraper
           student_hash[:blog] = link
         end
       end
-
       student_hash[:profile_quote] = doc.css(".profile-quote").text
       student_hash[:bio] = doc.css(".bio-content p").text
     end
